07d56c82f51eeceb2c99741ab6384356cd4bd646
neerlym <astrobits@yandex.ru>
2020-08-22 15:52:44 +0700

    В модуле get_file_ex.py изменены параметры функции get_file_ex(): get_file_ex(path, extension) -> [ ... ]. Теперь фнкция принимает путь до каталога и расширение, возвращает список всех файлов с указанным расширением в данном каталоге.

--- modules/get_file_ex.py
+++ modules/get_file_ex.py
@@ -1,21 +1,22 @@
 # -----------------------------------------------------------------------------
 # Модуль содержит функцию get_file_ex(), которая возвращает список
 # всех файлов в указанном каталоге с указанным расширением.
-# Если исходный список пуст, функция также возвращает пустой список.
+# Если в каталоге нет файлов, функция возвращает пустой список.
 # Если расширение указано как пустая строка "", функция возвращает список
 # файлов без расширения (если такие имеются, иначе возвращает пустой список).
-# Сигнатура : get_file_ex(file_list, extension) -> []
+# Сигнатура : get_file_ex(path, extension) -> [ ... ]
 # -----------------------------------------------------------------------------
 
 from get_extension import get_ex
 from get_file_list import get_file_list
 
 
-def get_file_ex(file_list, ex):
+def get_file_ex(path, ex):
+    file_list = get_file_list(path)
     ex_list = list(filter(lambda item: get_ex(item) == ex, file_list))
     return ex_list
 
 
 if __name__ == "__main__":
     print("*** module get_file_ex.py ***")
-    print(get_file_ex(get_file_list("./"), "py"))
+    print(get_file_ex("./", "py"))
