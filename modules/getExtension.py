2351cd279857da4dabd98ef54a0b348b44658e8f
neerlym <astrobits@yandex.ru>
2020-08-22 11:33:58 +0700

    Добавлен модуль "getExtension.py". Он содержит функцию get_ex(), возвращающую расширение по имени файла.

---
+++ modules/getExtension.py
@@ -0,0 +1,13 @@
+#
+# Функция get_ex() принимает имя файла и возвращает его расширение.
+# Сигнатура : get_ex(file_name) -> extension
+#
+
+def get_ex():
+    pass
+
+
+if __name__ == "__main__":
+    fileName = input("file = ")
+    ex = get_ex()
+    print("extension = " + "." + ex)
