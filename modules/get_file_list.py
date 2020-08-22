953b8b19f678dcfe7c36ce9b74e178a141d3142c
neerlym <astrobits@yandex.ru>
2020-08-22 19:57:54 +0700

    В модуле get_file_list.py изменили функцию get_file_list(), чтобы она возвращала дополнительно к имени файла дату его модификации и размер внутри каждого элемента возвращаемого списка.

--- modules/get_file_list.py
+++ modules/get_file_list.py
@@ -1,26 +1,34 @@
 # -----------------------------------------------------------------------------
 # Этот модуль содержит функцию get_file_list(), которая принимает путь до
 # каталога, а возвращает список файлов в данном каталоге.
+# Каждый элемент списка представляет собой список из 3-х элементов: имени
+# файла, даты и времени модификации, и размера.
 # Если файлов в данном каталоге нет, возвращается пустой список.
 # Сигнатура : get_file_list(path) -> []
 # -----------------------------------------------------------------------------
 
 import os
+import datetime
 
 
-def get_file_list(my_path):
-    if my_path[-1] != '/':
+def get_file_list(path):
+    if path[-1] != '/':
         # Модифицируем путь так, чтобы в конце он содержал косую черту.
         # Это нужно для простого добавления к пути имени файла/каталога.
-        my_path = my_path + "/"
+        path = path + "/"
     # Получаем список всех файлов и каталогов в заданном каталоге.
-    all_list = os.listdir(path=my_path)
+    all_list = os.listdir(path=path)
     # Формируем новый список и оставляем там только список файлов.
     # (каталоги не включаем)
     file_list = []
     for item in all_list:
-        if not os.path.isdir(my_path + item):
-            file_list.append(item)
+        file_path = path + item
+        if not os.path.isdir(file_path):
+            modified_date = datetime.datetime.fromtimestamp(
+                os.path.getmtime(file_path)
+            ).strftime("%Y-%m-%d %H:%M:%S")
+            size = str(os.path.getsize(file_path))
+            file_list.append([item, modified_date, size])
     return file_list
 
 
